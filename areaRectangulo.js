73c8c26d67bfa1c6f6cf3b51ac54d6cd39cfaaff
Natalia <nataliaspacienzia03@gmail.com>
2023-05-28 19:21:04 -0300

    agrerga practico0

---
+++ areaRectangulo.js
@@ -0,0 +1,8 @@
+function calcularAreaRectangulo(base, altura) {
+    let area = base * altura;
+    return area;
+}
+let base = 5;
+let altura = 3;
+let area = calcularAreaRectangulo(base, altura);
+console.log(area);
