ab42067f102e3d1a5dd44d993af1fc12d4f889d0
Natalia0013 <nataliaspacienzia03@gmail.com>
2023-06-13 20:56:28 -0300

    Actualizar miNombreCompleto.js

---
+++ practico1/miNombreCompleto.js
@@ -0,0 +1,17 @@
+// Parte 1: VARIABLES
+
+// 1 a. Defini una variable nombre
+let nombre;
+
+// 1 b. Guardar tu nombre real
+nombre = "Natalia Belen";
+
+//1 c. Defina y guarde en un mismo paso la variable apellido, y el valor tu ape. real
+let apellido = "Sosa Pacienzia";
+
+// 1 d. Definir y guardar, la edad y , designar edad real
+let edad = 30;
+
+// imprimir : "Hola, [nombre] [apellido]. Tienes [edad] años."
+console.log("Hola, " + nombre + " " + apellido +". Tienes " + edad + " años.");
+
